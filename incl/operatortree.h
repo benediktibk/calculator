7d036dea45983387af66d8ba76952c36b5d2548b
Benedikt Schmidt <benediktibk@aon.at>
2013-05-10 19:15:11 +0200

    implemented some useful functions for parsing an expression

--- incl/operatortree.h
+++ incl/operatortree.h
@@ -12,8 +12,17 @@ public:
 	~OperatorTree();
 
 	std::vector<const OperatorNode*> getNodesInOrder() const;
-	void parse(std::string expression);
 	bool parsingFailed() const;
+
+private:
+	void parse(std::string expression);
+	OperatorNode* parseRecursive(const std::string &expression);
+	std::string::const_iterator findOpeningBracket(const std::string &expression);
+	std::string::const_iterator findOpeningBracket(const std::string &expression, const std::string::const_iterator &start);
+	std::string::const_iterator findClosingBracket(const std::string &expression, const std::string::const_iterator &openingBracket);
+	std::vector<size_t> findMultiplicationsAndDivisions(const std::string &expression);
+	std::vector<size_t> findAdditionsAndSubtractions(const std::string &expression);
+	std::vector<size_t> findOperatorPositions(const std::string &expression, char operation);
 
 private:
 	OperatorNode *m_rootNode;
--- src/operatortree.cpp
+++ src/operatortree.cpp
@@ -24,6 +24,76 @@ std::vector<const OperatorNode*> OperatorTree::getNodesInOrder() const
 void OperatorTree::parse(string expression)
 {
 	remove(expression.begin(), expression.end(), ' ');
+	m_rootNode = parseRecursive(expression);
+}
+
+OperatorNode *OperatorTree::parseRecursive(const string &expression)
+{
+	if (expression.size() == 0)
+	{
+		m_parsingFailed = true;
+		return 0;
+	}
+}
+
+string::const_iterator OperatorTree::findOpeningBracket(const string &expression)
+{
+	return find(expression.begin(), expression.end(), '(');
+}
+
+string::const_iterator OperatorTree::findOpeningBracket(const string &expression, const string::const_iterator &start)
+{
+	return find(start, expression.end(), '(');
+}
+
+string::const_iterator OperatorTree::findClosingBracket(const string &expression, const string::const_iterator &openingBracket)
+{
+	unsigned openBrackets = 0;
+	for (string::const_iterator i = openingBracket; i != expression.end(); ++i)
+	{
+		if (*i == '(')
+			++openBrackets;
+		else if (*i == ')')
+			--openBrackets;
+
+		if (openBrackets == 0)
+			return i;
+	}
+
+	return expression.end();
+}
+
+vector<size_t> OperatorTree::findMultiplicationsAndDivisions(const string &expression)
+{
+	vector<size_t> multiplications = findOperatorPositions(expression, '*');
+	vector<size_t> divisions = findOperatorPositions(expression, '/');
+	vector<size_t> result;
+	result.reserve(multiplications.size() + divisions.size());
+	result.insert(result.end(), multiplications.begin(), multiplications.end());
+	result.insert(result.end(), divisions.begin(), divisions.end());
+	return result;
+}
+
+vector<size_t> OperatorTree::findAdditionsAndSubtractions(const string &expression)
+{
+	vector<size_t> additions = findOperatorPositions(expression, '+');
+	vector<size_t> subtractions = findOperatorPositions(expression, '-');
+	vector<size_t> result;
+	result.reserve(additions.size() + subtractions.size());
+	result.insert(result.end(), additions.begin(), additions.end());
+	result.insert(result.end(), subtractions.begin(), subtractions.end());
+	return result;
+}
+
+vector<size_t> OperatorTree::findOperatorPositions(const string &expression, char operation)
+{
+	vector<size_t> result;
+
+	for (size_t i = 0; i < expression.size(); ++i)
+		if (expression[i] == operation)
+			result.push_back(i);
+
+	return result;
 }
 
 bool OperatorTree::parsingFailed() const
