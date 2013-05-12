c883136854ae1a7a6f6be415e22ecc860723c186
Benedikt Schmidt <benediktibk@aon.at>
2013-05-12 13:41:15 +0200

    added short documentation to OperatorTree

--- incl/operatortree.h
+++ incl/operatortree.h
@@ -5,6 +5,20 @@
 #include <vector>
 #include "operatornode.h"
 
+/*!
+ * \brief parses and calculates a mathematical expression
+ *
+ * The operator tree is consctructed from a string, which contains a mathematical expression like 4+7*(4+3). This expression is
+ * therefore parsed and can be calculated afterwards with a call to calculateValue.
+ * If the parsing failed, because for example a bracket is missing, the flag parsingFailed is set to true.
+ * Internally the information is stored in a tree, where the leaves are single values. The nodes above executed the actual calculation.
+ * The order in which the expression needs to be calculated is stored through the position in the tree. The closer an operation
+ * is to the root node, the later it has to be evaluated. For example 7*(4+3)+4 will result in the following tree:
+ *           +
+ *         *   4
+ *       7  +
+ *         4 3
+ */
 class OperatorTree
 {
 public:
