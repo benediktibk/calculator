ff4d2f41ebecdbbfc55735d23741629d61431972
Markus Hofbauer <markus.hofbauer@tum.de>
2013-05-16 20:17:59 +0200

    implemented change function to change between deg and rad without unitcheck

--- src/unaryoperatornode.cpp
+++ src/unaryoperatornode.cpp
@@ -2,12 +2,14 @@
 #include "unaryoperatornode.h"
 #include <assert.h>
 #include <math.h>
+#include "constant.h"
 
 using namespace std;
 
 UnaryOperatorNode::UnaryOperatorNode(UnaryOperationType operationType) :
 	m_operationType(operationType),
-	m_node(0)
+    m_node(0),
+    m_changeToDegree(false)
 { }
 
 UnaryOperatorNode::~UnaryOperatorNode()
@@ -16,12 +18,22 @@ UnaryOperatorNode::~UnaryOperatorNode()
 	m_node = 0;
 }
 
+void UnaryOperatorNode::changeModeDegRad(bool mode)
+{
+    m_changeToDegree = mode;
+}
+
 double UnaryOperatorNode::getValue(bool &error) const
 {
 	assert(m_node != 0);
 
 	error = false;
 	double nodeValue = m_node->getValue(error);
+
+    if(m_changeToDegree == true)
+    {
+        nodeValue = nodeValue * 180 / (2*pi);
+    }
 
 	switch(m_operationType)
 	{
